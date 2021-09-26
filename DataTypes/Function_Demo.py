dfc1017bde897cd37cc5fd4f3a377ac6010cc72d
unknown <swativishnu@gmail.com>
2021-09-26 20:06:43 +0530

    diff demo

--- DataTypes/Function_Demo.py
+++ DataTypes/Function_Demo.py
@@ -5,8 +5,7 @@
 #
 #
 # function(1, 2, 3, 4, 5)
-#
-#
+
 # # Write a function calculation() such that it can accept two variables and calculate the addition
 # # and subtraction of them. And also it must return both addition and subtraction
 # # in a single return call
@@ -14,7 +13,7 @@
 #     return a + b, a - b
 
 
-# print(calculation(12, 4))
+# print(calculation(12, 5))
 
 # Create a function showEmployee() in such a way that it should accept employee name, and
 # its salary and display both.
